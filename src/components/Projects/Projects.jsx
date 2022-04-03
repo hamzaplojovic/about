f0c6e4ff711192a94d4d5406c0facce2262f8c99
hamzaplojovic <hamzaplojovic4@gmail.com>
2022-04-03 14:23:52 +0200

    added icons on projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,6 +10,7 @@ const Card = (props) => {
             data-aos={props.aos}
             data-aos-duration={props.duration}
         >
+            <img src={props.image} className="icon" />
             <h1>{props.name}</h1>
             <a className="link" href={"https://" + props.link}>
                 <h5>{props.link}</h5>
@@ -26,6 +27,7 @@ const ProjectsTablet = () => {
             <div className="cards cards-first">
                 <Card
                     name="hamzaplojovic"
+                    image="https://o.remove.bg/downloads/bc76262e-fb10-4fec-bb90-f3afd036096a/website-removebg-preview.png"
                     desc="This is about readme for github."
                     link="github.com/hamzaplojovic/hamzaplojovic"
                     aos="fade-down"
@@ -33,6 +35,7 @@ const ProjectsTablet = () => {
                 />
                 <Card
                     name="Lua"
+                    image="https://o.remove.bg/downloads/15cae946-5470-4064-b33d-1921373fa496/svg-removebg-preview.png"
                     desc="This is Lua course that i am going to."
                     link="github.com/hamzaplojovic/Lua"
                     aos="fade-down"
@@ -42,6 +45,7 @@ const ProjectsTablet = () => {
             <div className="cards">
                 <Card
                     name="Todo"
+                    image="https://o.remove.bg/downloads/f7ed6988-99d2-431a-8620-6cdb6f31a7c2/1560-removebg-preview.png"
                     desc="Todo app to make yourself more organized."
                     link="github.com/hamzaplojovic/React-Todo"
                     aos="fade-down"
@@ -49,6 +53,7 @@ const ProjectsTablet = () => {
                 />
                 <Card
                     name="Auto Clicker"
+                    image="https://o.remove.bg/downloads/190b277d-85d1-40c7-aa3f-c5b6d942c4d9/1676719-200-removebg-preview.png"
                     desc="This is python bot that clicks 100 cps"
                     link="github.com/hamzaplojovic/auto-clicker"
                     aos="fade-up"
@@ -58,6 +63,7 @@ const ProjectsTablet = () => {
             <div className="cards">
                 <Card
                     name="mojnovipazar"
+                    image="https://o.remove.bg/downloads/16bde039-05a5-486b-b323-8ca92ca4e17a/APFygbLmxlXdAAAAAElFTkSuQmCC-removebg-preview.png"
                     desc="Website for the old memorials of Novi Pazar."
                     link="github.com/hamzaplojovic/mojovipazar-rs"
                     aos="fade-up"
@@ -65,22 +71,7 @@ const ProjectsTablet = () => {
                 />
                 <Card
                     name="Travello"
-                    desc="Template for travel company."
-                    link="github.com/hamzaplojovic/Travel-Website"
-                    aos="fade-up"
-                    duration="1700"
-                />
-            </div>
-            <div className="cards">
-                <Card
-                    name="Github API"
-                    desc="Github profile page."
-                    link="github.com/hamzaplojovic/GithubAPI"
-                    aos="fade-up"
-                    duration="1500"
-                />
-                <Card
-                    name="Travello"
+                    image="https://o.remove.bg/downloads/c2ede8e1-293c-491d-b87b-46790c78f52a/svg-removebg-preview.png"
                     desc="Template for travel company."
                     link="github.com/hamzaplojovic/Travel-Website"
                     aos="fade-up"
@@ -98,6 +89,7 @@ const ProjectsPhone = () => {
             <div className="cards">
                 <Card
                     name="hamzaplojovic"
+                    image="https://o.remove.bg/downloads/bc76262e-fb10-4fec-bb90-f3afd036096a/website-removebg-preview.png"
                     desc="This is about readme for github."
                     link="github.com/hamzaplojovic/hamzaplojovic"
                     aos="fade-down"
@@ -107,6 +99,7 @@ const ProjectsPhone = () => {
             <div className="cards">
                 <Card
                     name="Lua"
+                    image="https://o.remove.bg/downloads/15cae946-5470-4064-b33d-1921373fa496/svg-removebg-preview.png"
                     desc="This is Lua course that i am going to."
                     link="github.com/hamzaplojovic/Lua"
                     aos="fade-down"
@@ -116,6 +109,7 @@ const ProjectsPhone = () => {
             <div className="cards">
                 <Card
                     name="Todo"
+                    image="https://o.remove.bg/downloads/f7ed6988-99d2-431a-8620-6cdb6f31a7c2/1560-removebg-preview.png"
                     desc="Todo app to make yourself more organized."
                     link="github.com/hamzaplojovic/React-Todo"
                     aos="fade-down"
@@ -125,6 +119,7 @@ const ProjectsPhone = () => {
             <div className="cards">
                 <Card
                     name="Auto Clicker"
+                    image="https://o.remove.bg/downloads/190b277d-85d1-40c7-aa3f-c5b6d942c4d9/1676719-200-removebg-preview.png"
                     desc="This is python bot that clicks 100 cps"
                     link="github.com/hamzaplojovic/auto-clicker"
                     aos="fade-up"
@@ -134,6 +129,7 @@ const ProjectsPhone = () => {
             <div className="cards">
                 <Card
                     name="mojnovipazar"
+                    image="https://o.remove.bg/downloads/16bde039-05a5-486b-b323-8ca92ca4e17a/APFygbLmxlXdAAAAAElFTkSuQmCC-removebg-preview.png"
                     desc="Website for the old memorials of Novi Pazar."
                     link="github.com/hamzaplojovic/mojovipazar-rs"
                     aos="fade-up"
@@ -143,6 +139,7 @@ const ProjectsPhone = () => {
             <div className="cards">
                 <Card
                     name="Travello"
+                    image="https://o.remove.bg/downloads/c2ede8e1-293c-491d-b87b-46790c78f52a/svg-removebg-preview.png"
                     desc="Template for travel company."
                     link="github.com/hamzaplojovic/Travel-Website"
                     aos="fade-up"
@@ -164,6 +161,7 @@ function Projects() {
                 <h1 className="about-heading">Projects</h1>
                 <div className="cards">
                     <Card
+                        image="https://o.remove.bg/downloads/bc76262e-fb10-4fec-bb90-f3afd036096a/website-removebg-preview.png"
                         name="hamzaplojovic"
                         desc="This is about readme for github."
                         link="github.com/hamzaplojovic/hamzaplojovic"
@@ -172,6 +170,7 @@ function Projects() {
                     />
                     <Card
                         name="Lua"
+                        image="https://o.remove.bg/downloads/15cae946-5470-4064-b33d-1921373fa496/svg-removebg-preview.png"
                         desc="This is Lua course that i am going to."
                         link="github.com/hamzaplojovic/Lua"
                         aos="fade-down"
@@ -179,6 +178,7 @@ function Projects() {
                     />
                     <Card
                         name="Todo"
+                        image="https://o.remove.bg/downloads/f7ed6988-99d2-431a-8620-6cdb6f31a7c2/1560-removebg-preview.png"
                         desc="Todo app to make yourself more organized."
                         link="github.com/hamzaplojovic/React-Todo"
                         aos="fade-down"
@@ -188,6 +188,7 @@ function Projects() {
                 <div className="cards">
                     <Card
                         name="Auto Clicker"
+                        image="https://o.remove.bg/downloads/190b277d-85d1-40c7-aa3f-c5b6d942c4d9/1676719-200-removebg-preview.png"
                         desc="This is python bot that clicks 100 cps"
                         link="github.com/hamzaplojovic/auto-clicker"
                         aos="fade-up"
@@ -195,12 +196,14 @@ function Projects() {
                     />
                     <Card
                         name="mojnovipazar"
+                        image="https://o.remove.bg/downloads/16bde039-05a5-486b-b323-8ca92ca4e17a/APFygbLmxlXdAAAAAElFTkSuQmCC-removebg-preview.png"
                         desc="Website for the old memorials of Novi Pazar."
                         link="github.com/hamzaplojovic/mojovipazar-rs"
                         aos="fade-up"
                         duration="1500"
                     />
                     <Card
+                        image="https://o.remove.bg/downloads/c2ede8e1-293c-491d-b87b-46790c78f52a/svg-removebg-preview.png"
                         name="Travello"
                         desc="Template for travel company."
                         link="github.com/hamzaplojovic/Travel-Website"
