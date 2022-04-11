6686acc8bdd8fe8be6c59f0e8ee175c081159388
hamzaplojovic <hamzaplojovic4@gmail.com>
2022-04-11 21:00:56 +0200

    Changed home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,12 +4,6 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Home = (props) => {
-    setTimeout(() => {
-        window.scrollTo({
-            top: 700,
-            behavior: "smooth",
-        });
-    }, 1000);
     useEffect(() => {
         AOS.init();
         AOS.refresh();
