2745df87032b24c913c207232eb2e847593d9dba
hamzaplojovic <hamzaplojovic4@gmail.com>
2022-04-11 19:07:17 +0200

    Added auto scroll on home after 1 second

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,12 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Home = (props) => {
+    setTimeout(() => {
+        window.scrollTo({
+            top: 700,
+            behavior: "smooth",
+        });
+    }, 1000);
     useEffect(() => {
         AOS.init();
         AOS.refresh();
